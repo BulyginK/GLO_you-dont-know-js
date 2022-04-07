2944771663ffd892049ffa12c5b6debdd42928ce
BulyginK <bulygin.k@gmail.com>
2022-04-07 22:11:35 +0300

    Выполнил 10 задание

--- script.js
+++ script.js
@@ -2,25 +2,28 @@ const book = document.querySelectorAll('.book');
 const body = document.querySelector('body');
 const title = document.querySelectorAll('[target="_blank"]');
 const banner = document.querySelector('.adv');
+const liBook2 = book[0].querySelectorAll('li');
+const liBook5 = book[5].querySelectorAll('li');
+const liBook6 = book[2].querySelectorAll('li');
+const newСhapterBook6 = liBook6[8].cloneNode(true);
 
-// 1.Восстановить порядок книг
 book[0].before(book[1]);
 book[3].before(book[4]);
 book[5].after(book[2]);
 
-// 2.Заменить картинку заднего фона на другую из папки image
 body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
-// 3.Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
 title[4].innerHTML = 'Книга 3. this и Прототипы Объектов';
 
-// 4.Удалить рекламу со страницы
 banner.style.display = 'none';
 
-// 5.Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+liBook2[4].before(liBook2[6]);
+liBook2[4].before(liBook2[8]);
+liBook2[10].before(liBook2[2]);
+liBook5[3].before(liBook5[9]);
+liBook5[6].before(liBook5[2]);
+liBook5[8].before(liBook5[5]);
 
-
-
-
-console.log(title);
+newСhapterBook6.innerHTML = 'Глава 8: За пределами ES6'
+liBook6[8].after(newСhapterBook6);
 
