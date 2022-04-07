4c262992016a9ffd0ce8c7a3652dc21cef15ac32
BulyginK <buligin.k@gmail.com>
2022-04-07 17:47:47 +0300

    Выполнил четыре пункта

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const book = document.querySelectorAll('.book');
 const body = document.querySelector('body');
 const title = document.querySelectorAll('[target="_blank"]');
+const banner = document.querySelector('.adv');
 
 // 1.Восстановить порядок книг
 book[0].before(book[1]);
@@ -12,6 +13,13 @@ body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
 // 3.Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
 title[4].innerHTML = 'Книга 3. this и Прототипы Объектов';
+
+// 4.Удалить рекламу со страницы
+banner.style.display = 'none';
+
+// 5.Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+
+
 
 
 console.log(title);
